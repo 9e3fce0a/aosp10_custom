482b3dca23fbf5963a372b3ae0aad6a7692b09ed
9e3fce0a <9e3fce0a@users.noreply.github.com>
2019-12-13 12:33:28 +0000

    Experiment

--- 00002-custom-boot-animation.sh
+++ 00002-custom-boot-animation.sh
@@ -5,4 +5,4 @@ BOOT_ANIMATION="https://github.com/9e3fce0a/microg/raw/master/bootanimation.zip"
 mkdir -p $BUILD_DIR/branding
 echo "Downloading boot animation $BOOT_ANIMATION"
 wget -O $BUILD_DIR/branding/bootanimation.zip $BOOT_ANIMATION
-sed -i "\$aPRODUCT_COPY_FILES += branding/bootanimation.zip:system/media/bootanimation.zip" ${BUILD_DIR}/build/make/target/product/handheld_product.mk
+sed -i "\$aPRODUCT_COPY_FILES += branding/bootanimation.zip:system/media/bootanimation.zip" ${BUILD_DIR}/build/make/target/product/handheld_system.mk
